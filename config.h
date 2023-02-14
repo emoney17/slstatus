fc78077132c709990fa2eb9ca9d45830dba31dca
emoney17 <emmanuel.million@tutanota.com>
2023-02-14 16:37:19 -0600

    spacing

--- config.h
+++ config.h
@@ -65,11 +65,11 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ ipv6,	     "[%s]  ",     "enp7s0"},
-	{ ipv4,	     "[%s]  ",     "enp7s0"},
-	{ disk_free, "[%s]  ",      "/"},
-	{ cpu_perc,  "[CPU   %s%%]  ", NULL},
-	{ ram_perc,  "[RAM   %s%%]  ", NULL},
-	{ ram_used,  "[%s]  ",       NULL},
+	{ ipv6,	     "[%s] ",     "enp7s0"},
+	{ ipv4,	     "[%s] ",     "enp7s0"},
+	{ disk_free, "[%s] ",      "/"},
+	{ cpu_perc,  "[CPU   %s%%] ", NULL},
+	{ ram_perc,  "[RAM   %s%%] ", NULL},
+	{ ram_used,  "[%s] ",       NULL},
 	{ datetime, "%s",           "%a %b %d %r" },
 };
